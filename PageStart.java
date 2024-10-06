8ca36305058e3d6a51c76280db0c7b6434e2e49e
POOMIX <92581505+POOMIX@users.noreply.github.com>
2024-10-06 13:09:27 +0700

    Update_pageStart
    ยังไม่เสร็จ แต่ เพิ่ม รูปต่างๆแล้ว

--- PageStart.java
+++ PageStart.java
@@ -1,11 +1,27 @@
-import java.awt.Toolkit;
-
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class PageStart extends JPanel{
     private App app;
     PageStart(App app){
-        this.add.showPanel("menu");
+        this.app = app;
+        
+    }
+    public void paint(Graphics g){
+        ImageIcon icon = new ImageIcon("./image/bg-start.png");
+        ImageIcon icon_setting = new ImageIcon("./image/settings-white.png");
+        ImageIcon icon_order = new ImageIcon("./image/checklist-white.png");
+        ImageIcon icon_shop = new ImageIcon("./image/shop -white.png");
+        ImageIcon icon_dish = new ImageIcon("./image/dish.png");
+        ImageIcon icon_thermometer = new ImageIcon("./image/thermometer.png");
+        super.paint(g);
+        g.drawImage(icon.getImage(), 0,0, this);
+        g.drawImage(icon_setting.getImage(), 0,0,50,50, this);
+        g.drawImage(icon_order.getImage(), 0,70,50,50, this);
+        g.drawImage(icon_shop.getImage(), 0,140,50,50, this);
+        g.drawImage(icon_dish.getImage(), 1000,-50,500,500, this);
+        g.drawImage(icon_thermometer.getImage(), 0,310,359,91, this);
     }
 
 }
--- Run.java
+++ Run.java
@@ -2,9 +2,11 @@ class Run {
     public static void main(String[] args){
         App app = new App("The Meat");
         PageMenu menu = new PageMenu();
+        PageStart start = new PageStart(app);
         Sound sound = new Sound();
         app.addPanel(menu,"menu");
-        app.showPanel("menu");
+        app.addPanel(start,"start");
+        app.showPanel("start");
         
     }    
 }
