d48bdcba03636891cc6eec7329c31890105b7165
POOMIX <92581505+POOMIX@users.noreply.github.com>
2024-10-08 02:16:56 +0700

    Up-date_test_thread
    ยังคงไม่สมบูรณ์

--- PageStart.java
+++ PageStart.java
@@ -1,20 +1,31 @@
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.*;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
 
 public class PageStart extends JPanel {
     private App app;
     private JButton B_setting;
     private JButton B_order;
     private JButton B_shop;
+    private Rectangle meatRect;
+    private Rectangle plateRect;
+    private MeatThread meatThread;
+    private boolean isHoldingMeat = false;
+    private Point lastMousePosition;
     
     PageStart(App app) {
         this.app = app;
         setLayout(null);
-    
+        
+        // สร้างพื้นที่ชิ้นเนื้อและจาน
+        meatRect = new Rectangle(402, 160, 500, 382); // ขนาดและตำแหน่งชิ้นเนื้อ
+        plateRect = new Rectangle(1000, -50, 500, 500); // ขนาดและตำแหน่งจาน
+
         // สร้างปุ่ม setting
         B_setting = new JButton();
         B_setting.setBounds(0, 0, 50, 50);
@@ -24,11 +35,10 @@ public class PageStart extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("setting Clicked");
-                // app.showPanel("setting");
             }
         });
         add(B_setting);
-    
+
         // สร้างปุ่ม order
         B_order = new JButton("Order");
         B_order.setBounds(0, 70, 50, 50);
@@ -38,11 +48,10 @@ public class PageStart extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Order Clicked");
-                // app.showPanel("order");
             }
         });
         add(B_order);
-    
+        
         // สร้างปุ่ม shop
         B_shop = new JButton("Shop");
         B_shop.setBounds(0, 140, 50, 50);
@@ -52,31 +61,97 @@ public class PageStart extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Shop Clicked");
-                // app.showPanel("shop");
             }
         });
         add(B_shop);
+        
+        // เริ่มการทำงานของ MeatThread
+        meatThread = new MeatThread(this, meatRect);
+        meatThread.start();
+        
+        // Mouse listener เพื่อตรวจจับการลากชิ้นเนื้อ
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (meatRect.contains(e.getPoint())) {
+                    isHoldingMeat = true;
+                    lastMousePosition = e.getPoint();
+                }
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (isHoldingMeat) {
+                    isHoldingMeat = false;
+                    // ตรวจสอบว่าชิ้นเนื้อชนกับจานหรือไม่
+                    if (meatRect.intersects(plateRect)) {
+                        System.out.println("Finish! Meat on plate.");
+                    }
+                }
+            }
+        });
+
+        // Mouse motion listener เพื่อลากชิ้นเนื้อ
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (isHoldingMeat) {
+                    int dx = e.getX() - lastMousePosition.x;
+                    int dy = e.getY() - lastMousePosition.y;
+                    meatRect.x += dx;
+                    meatRect.y += dy;
+                    lastMousePosition = e.getPoint();
+                    repaint(); // รีเฟรชจอภาพเมื่อมีการลากเนื้อ
+                }
+            }
+        });
     }
-    
-    
-    
+
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
+        
+        // วาด background และปุ่มต่างๆ
         ImageIcon icon_setting = new ImageIcon("./image/settings-white.png");
         ImageIcon icon = new ImageIcon("./image/bg-start.png");
         ImageIcon icon_order = new ImageIcon("./image/checklist-white.png");
         ImageIcon icon_shop = new ImageIcon("./image/shop -white.png");
         ImageIcon icon_dish = new ImageIcon("./image/dish.png");
-        ImageIcon icon_thermometer = new ImageIcon("./image/thermometer.png");
-        ImageIcon icon_Rank = new ImageIcon("./image/rank.png");
+        ImageIcon icon_meat = new ImageIcon("./image/rare.png");
         
-        super.paint(g);
         g.drawImage(icon.getImage(), 0, 0, this);
         g.drawImage(icon_setting.getImage(), 0, 0, 50, 50, this);
         g.drawImage(icon_order.getImage(), 0, 70, 50, 50, this);
         g.drawImage(icon_shop.getImage(), 0, 140, 50, 50, this);
-        g.drawImage(icon_dish.getImage(), 1000, -50, 500, 500, this);
-        g.drawImage(icon_thermometer.getImage(), 0, 310, 359, 91, this);
-        g.drawImage(icon_Rank.getImage(), 980, 400, 287, 304, this);
+        g.drawImage(icon_dish.getImage(), plateRect.x, plateRect.y, plateRect.width, plateRect.height, this); // วาดจาน
+        g.drawImage(icon_meat.getImage(), meatRect.x, meatRect.y, meatRect.width, meatRect.height, this); // วาดชิ้นเนื้อ
+    }
+}
+
+// คลาสสำหรับจัดการการทำงานของเทรด
+class MeatThread extends Thread {
+    private Rectangle meatRect;
+    private JPanel panel;
+    private boolean isRunning = true;
+
+    public MeatThread(JPanel panel, Rectangle meatRect) {
+        this.panel = panel;
+        this.meatRect = meatRect;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                Thread.sleep(16); // อัพเดตทุก 16 ms (60 FPS)
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            panel.repaint(); // รีเฟรชหน้าจอ
+        }
+    }
+
+    public void stopRunning() {
+        isRunning = false; // หยุดการทำงานของเทรด
     }
 }
