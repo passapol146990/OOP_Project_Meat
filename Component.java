286dc1df56548b838b888069076028afcffbbbf4
Nitiphumi Dibprakhon <nitiphumidibprakhon@gmail.com>
2024-10-19 00:32:50 +0700

    update 1.3.2
    เพิ่มปุ่มมน ตัวอักษร

--- Component.java
+++ Component.java
@@ -1,3 +1,37 @@
-public class Component {
-    
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.awt.*;
+
+public class Component extends JButton{
+    public Component(String text,Color color) {
+        super(text);
+        setContentAreaFilled(false);    
+        setFocusPainted(false);
+        setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+        setBackground(color);
+        setCursor(new Cursor(JFrame.HAND_CURSOR));
+    }
+    // โอเวอร์ไลด์เมธอดที่ถูกเรียกมาจาก components JButton
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        // วาดปุ่ม
+        // ถ้ากดปุ่มจะให้สีเปลี่ยน
+        if (getModel().isArmed()) {
+            g2.setColor(new Color(255,192,203));
+        }else{
+            g2.setColor(getBackground());
+        }
+        // เซ็ตพื้นหลังให้เป็นสีต่างๆ
+        g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+        //วาดขอบ
+        g2.setColor(Color.BLACK);
+        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+        // g2.dispose();
+        super.paintComponent(g);
+    }
+    public void updateUI() {
+        setUI(new BasicButtonUI());
+    }
 }
--- PageSeting.java
+++ PageSeting.java
@@ -5,7 +5,8 @@ public class PageSeting extends JPanel {
 
     private JSlider musicSlider;
     private JSlider audioSlider;
-
+    private JLabel  music;
+    private JLabel  audio;
     public PageSeting() {
         setLayout(null);
 
@@ -16,18 +17,22 @@ public class PageSeting extends JPanel {
         panel.setBackground(Color.blue);
         
         // สร้างปุ่ม "Back to the menu"
-        JButton backButton = new JButton("Back to the menu");
+        Component backButton = new Component("Back to the menu",new Color(254,169,169));
         backButton.setBounds(480, 530, 300, 50);
         add(backButton);
 
         // สร้าง JSlider สำหรับ music และ audio
         musicSlider = new JSlider(0, 100, 50);
         musicSlider.setBounds(300, 100, 500, 50);  // ตั้งตำแหน่ง slider
+        music = new JLabel("Music");
+        music.setBounds(150, 50, 1000, 50);
+        music.setFont(new Font("Courier New",Font.BOLD,50));
         audioSlider = new JSlider(0, 100, 50);
         audioSlider.setBounds(300, 250, 500, 50);
 
         // เพิ่ม slider และ panel ในแผง
         panel.add(musicSlider);
+        panel.add(music);
         panel.add(audioSlider);
         add(panel);
         
@@ -44,26 +49,13 @@ public class PageSeting extends JPanel {
 
         // ตรวจสอบว่าภาพสามารถโหลดได้หรือไม่
         ImageIcon bannerIcon = new ImageIcon("./image/BannerSeting.png");
-        if (bannerIcon.getImage() != null) {
-            g.drawImage(bannerIcon.getImage(), 300, 0, this);
-        } else {
-            System.out.println("Failed to load BannerSeting.png");
-        }
-
+         g.drawImage(bannerIcon.getImage(), 300, 0, this);
         // วาดไอคอนสำหรับ music และ audio
         ImageIcon musicIcon = new ImageIcon("./image/music.png");
-        if (musicIcon.getImage() != null) {
-            g.drawImage(musicIcon.getImage(), 225, 250, 50, 50, this);
-        } else {
-            System.out.println("Failed to load music.png");
-        }
-
+        g.drawImage(musicIcon.getImage(), 225, 250, 50, 50, this);
         ImageIcon volumeIcon = new ImageIcon("./image/volume.png");
-        if (volumeIcon.getImage() != null) {
-            g.drawImage(volumeIcon.getImage(), 225, 450, 50, 50, this);
-        } else {
-            System.out.println("Failed to load volume.png");
-        }
+        g.drawImage(volumeIcon.getImage(), 225, 450, 50, 50, this);
+           
     }
 }
 
