e374c86bd8da8002dab6c4425249998af39322e1
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-10-22 01:17:29 +0700

    update

--- Run.java
+++ Run.java
@@ -4,10 +4,10 @@ class Run {
         App app = new App("The Meat",baseClient);
         PageMenu menu = new PageMenu(app);
         PageStart start = new PageStart(app);
-        // PageSeting seting = new PageSeting(sound,app);
+        PageSeting seting = new PageSeting(app);
         app.addPanel(menu,"menu");
         app.addPanel(start,"start");
-        // app.addPanel(seting, "seting");
+        app.addPanel(seting, "seting");
         app.showPanel("start");
         // app.showPanel("menu");
         // app.showPanel("seting");
