57b72cd0d7231ee79546d50201baabb9f90a785f
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-10-22 01:22:54 +0700

    set up now

--- Run.java
+++ Run.java
@@ -5,9 +5,11 @@ class Run {
         PageMenu menu = new PageMenu(app);
         PageStart start = new PageStart(app);
         PageSeting seting = new PageSeting(app);
+        PageAbout about = new PageAbout(app);
         app.addPanel(menu,"menu");
         app.addPanel(start,"start");
         app.addPanel(seting, "seting");
+        app.addPanel(about, "about");
         app.showPanel("start");
         // app.showPanel("menu");
         // app.showPanel("seting");
